a52a70fdc8893b8fb0ace44361632c5aabe3286a
wilantury <ventas@antury.net>
2021-03-26 17:08:06 -0500

    [ADD] sort and chect repeated numbers

--- app/bundles/sort_array/sort_array.go
+++ app/bundles/sort_array/sort_array.go
@@ -27,32 +27,72 @@ func response(w http.ResponseWriter, data *SortArrayHandle, message string, http
 	w.Write(jsonResp)
 }
 
-func sortArray(unsortedArray []int) {
-	if len(unsortedArray) > 1 {
-		var mid int = len(unsortedArray) / 2
-		left := unsortedArray[:mid]
-		right := unsortedArray[mid:]
-		//divide and conquer: until length of array is less than 2
-		sortArray(left)
-		sortArray(right)
+func Merge(left, right []int) []int {
+	sortArray := make([]int, 0, len(left)+len(right))
 
-		//sort each array
-		i, j, k := 0, 0, 0
-
-		for i < len(left) && j < len(right) {
-
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) == 0 {
+			return append(sortArray, right...)
+		}
+		if len(right) == 0 {
+			return append(sortArray, left...)
+		}
+		if left[0] <= right[0] {
+			sortArray = append(sortArray, left[0])
+			left = left[1:]
+		} else {
+			sortArray = append(sortArray, right[0])
+			right = right[1:]
 		}
 	}
+	return sortArray
+}
+
+func MergeSort(unsortedArray []int) []int {
+	if len(unsortedArray) <= 1 {
+		return unsortedArray
+	}
+	mid := len(unsortedArray) / 2
+	left := MergeSort(unsortedArray[:mid])
+	right := MergeSort(unsortedArray[mid:])
+	return Merge(left, right)
+}
+
+func separateRepeated(sortedArray []int) []int {
+	var repeat []int
+	index := 0
+	for i := 0; i < len(sortedArray)-1; i++ {
+		if sortedArray[i] == sortedArray[i+1] {
+			repeat = append(repeat, sortedArray[i])
+		} else {
+			sortedArray[index] = sortedArray[i]
+			if i == len(sortedArray)-2 {
+				index++
+				sortedArray[index] = sortedArray[i+1]
+			}
+			index++
+		}
+	}
+
+	for _, val := range repeat {
+		sortedArray[index] = val
+		index++
+	}
+	return sortedArray
 }
 
 func (sa *SortArrayHandle) SortArrayView(w http.ResponseWriter, r *http.Request) {
 	var sortRes SortArrayHandle
 	decoder := json.NewDecoder(r.Body)
 	err := decoder.Decode(&sortRes)
+	//sort array
+	sol := MergeSort(sortRes.Unsorted)
+	sortRes.Sorted = separateRepeated(sol)
+
 	if err != nil {
 		response(w, &sortRes, "error to decode body", http.StatusInternalServerError)
 		return
 	}
-	fmt.Println(sortRes)
+
 	response(w, &sortRes, "Success", http.StatusOK)
 }
